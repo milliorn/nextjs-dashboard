53ed2d83392d535c4c498270eda627db98b9772d
milliorn <scottmilliorn@gmail.com>
2024-03-08 17:35:58 -0800

    Fetching data for the dashboard overview page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,30 @@
-export default function Page() {
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>This is the dashboard.</p>
-    </div>
+    <main>
+      <h1
+        className={`${lusitana.className} mb-4 border-8 border-black text-xl md:text-2xl`}
+      >
+        Dashboard
+      </h1>
+      <div className="grid gap-6 border-8 border-black sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 border-8 border-black md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
   );
 }
