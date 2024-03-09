ea71b5cfea241eef2897a7dba2d5ef5460190acc
milliorn <scottmilliorn@gmail.com>
2024-03-08 20:00:15 -0800

    Making the dashboard dynamic

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,10 +9,13 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // This is useful when you want to ensure that the data is always fresh.
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -33,6 +36,8 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +58,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -70,10 +77,10 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const numberOfInvoices = Number(data[ 0 ].rows[ 0 ].count ?? '0');
+    const numberOfCustomers = Number(data[ 1 ].rows[ 0 ].count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[ 2 ].rows[ 0 ].paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[ 2 ].rows[ 0 ].pending ?? '0');
 
     return {
       numberOfCustomers,
@@ -92,6 +99,8 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -124,6 +133,8 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -136,7 +147,7 @@ export async function fetchInvoicesPages(query: string) {
       invoices.status ILIKE ${`%${query}%`}
   `;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(Number(count.rows[ 0 ].count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
@@ -145,6 +156,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -162,7 +175,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    return invoice[0];
+    return invoice[ 0 ];
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
@@ -188,6 +201,8 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore(); // Add noStore() here to prevent the response from being cached.
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -223,7 +238,7 @@ export async function fetchFilteredCustomers(query: string) {
 export async function getUser(email: string) {
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0] as User;
+    return user.rows[ 0 ] as User;
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
