44d015a9d7d0967e3acdbec3e15b150d67a495c7
milliorn <scottmilliorn@gmail.com>
2024-03-10 21:45:18 -0700

    Creating an invoice

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+/**
+ * Renders the page for creating a new invoice.
+ * Fetches the list of customers and renders the form component.
+ * @returns The JSX element representing the page.
+ */
+export default async function Page() {
+  const customers = await fetchCustomers(); 
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
